793c0d17e2d3423e99fa06b230743e64ae0e9359
jed15 <jeremydunsmore@cmail.carleton.ca>
2013-07-10 16:23:00 -0400

    some problems fixed

--- follow.php
+++ follow.php
@@ -38,22 +38,38 @@
 				echo html_writer::link($from,'Go back');
 			echo $OUTPUT->box_end();
 			echo $OUTPUT->footer();
+		}else{
+		
+			//check if the use is already following the author
+			if(socialwiki_is_following($USER->id,$user2,$subwiki)){
+				//delete the record if the user is already following the author
+				socialwiki_unfollow($USER->id,$user2);
+			}else{
+				//if the user isn't following the author add a new follow
+				$record=new StdClass();
+				$record->userfromid=$USER->id;
+				$record->usertoid=$user2;
+				$record->subwikiid=$subwiki->id;
+				$DB->insert_record('socialwiki_follows',$record);
+			}
 		}
 		
-	}
-		if($user2==-1){
-			print_error('nouser','socialwiki');
+	}elseif($user2!=-1){
+
+		//check if the use is already following the author
+		if(socialwiki_is_following($USER->id,$user2,$subwiki)){
+			//delete the record if the user is already following the author
+			socialwiki_unfollow($USER->id,$user2);
+		}else{
+			//if the user isn't following the author add a new follow
+			$record=new StdClass();
+			$record->userfromid=$USER->id;
+			$record->usertoid=$user2;
+			$record->subwikiid=$subwiki->id;
+			$DB->insert_record('socialwiki_follows',$record);
 		}
-	//check if the use is already following the author
-	if(socialwiki_is_following($USER->id,$user2)){
-		//delete the record if the user is already following the author
-		socialwiki_unfollow($USER->id,$user2);
 	}else{
-		//if the user isn't following the author add a new follow
-		$record=new StdClass();
-		$record->userfromid=$USER->id;
-		$record->usertoid=$user2;
-		$DB->insert_record('socialwiki_follows',$record);
+		print_error('nouser','socialwiki');
 	}
 	//go back to the page you came from
 	redirect($from);
