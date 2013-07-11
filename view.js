efecae3b04d37623164a334c7c2420359ef0d934
Cameron Blanchard <ctblanch@gmail.com>
2013-07-11 16:35:00 -0400

    Removed conflict files

--- view.js
+++
@@ -1,8 +0,0 @@
-console.log("view.js included");
-
-$(document).ready(function(){
-    
-    $("#socialwiki_backbutton").css("display", "inline-block");
-    $("#socialwiki_forwardbutton").css("display", "inline-block");
-    
-    });
