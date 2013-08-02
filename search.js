81361acef9077ff7548befb5c5847cda3ff8e998
jed15 <jeremydunsmore@cmail.carleton.ca>
2013-08-02 13:37:40 -0400

    weight widget added for list view

--- search.js
+++ search.js
@@ -1,7 +1,11 @@
-var pattern1=/option=1/;
-var pattern2=/otion=2/;
+//patterns to determine the view mode
+var pattern1=/option=1/; 
+var pattern2=/option=2/;
 var pattern3=/option/;
-function liked(peer,pageid){
+var pattern4=/option=3/;
+
+
+function likedTree(peer,pageid){
 	$.ajax({
 		url:'ajax.php',
 		type:'get',
@@ -35,19 +39,35 @@ function rescoreTree (){
 		//readjust score
 		for(j in peers){
 			//add peer's score if they like the page
-			liked(peers[j],searchTree.nodes[i].id.substr(1))		
+			likedTree(peers[j],searchTree.nodes[i].id.substr(1))		
 		}
 	}
 }
 
+function rescorePages(){
+	var jpages=JSON.stringify(pages);
+	var jpeers=JSON.stringify(peers);
+	$.ajax({
+			url:'ajax.php',
+			type:'post',
+			data: {action:'pages',pages:jpages,peers:jpeers},
+			success: function(output){
+				$('.socialwiki_editor').html(output);
+			}
+		});
+}
+
 //function for weight sliders
 function weightSliders (divID){
+	//array containing labels for sliders
+	var labels={like:'like similarity',follow:'follow similarity',trust:'trust<br/>&nbsp',popular:'popularity<br/>&nbsp'};
 	$("#"+divID).prepend('<div id="sliders"></div>');
+	$('#sliders').append('<h2>Adjust Peer Weights<h2>');
 	//set up the sliders for each weighted score
 	for(option in scale){
 		$('#sliders').append('<div id="'+option+'container" class="slidecontainer"></div>')
 		$('#'+option+'container').append('<div id="'+option+'"class="socialwiki_slider"></div>');
-		$('#'+option+'container').append('<span>'+option+'</span>');
+		$('#'+option+'container').append('<span>'+labels[option]+'</span>');
 		$('#'+option).slider({
 		  orientation: "vertical",
 		  range: "min",
@@ -56,17 +76,24 @@ function weightSliders (divID){
 		  step:1,
 		  value:scale[option],
 		  slide:function( event, ui ) {
+				//update the scale
 				scale[this.id]=ui.value;
+				//update peer scores with new scale
 				for(i in peers){
 					peers[i].score=peers[i].trust*scale['trust']+peers[i].likesim*scale['like']+peers[i].followsim*scale['follow']+peers[i].popularity*scale['popular'];
 				}
-				rescoreTree();
-				//delete the old tree
-				$('#tree_container_div').remove();
-				$("#tree_zoommessage").remove();
-				$('#instruction').remove();
-				mTree = new TreeControl(searchTree, "socialwiki_content_area");
-				mTree.display();
+				if(pattern1.test(document.URL)||!pattern3.test(document.URL)){
+					//rescore and display the tree
+					rescoreTree();
+					//delete the old tree
+					$('#tree_container_div').remove();
+					$("#tree_zoommessage").remove();
+					$('#instruction').remove();
+					mTree = new TreeControl(searchTree, "socialwiki_content_area");
+					mTree.display();
+				}else if(pattern2.test(document.URL)){
+					rescorePages();
+				}
 			}
 		});
 	}
@@ -84,11 +111,12 @@ if(pattern1.test(document.URL)||!pattern3.test(document.URL)){
 
 $(document).ready(function() {
 	$(".phptree").css("display", "none");
-	
+	if(!pattern4.test(document.URL)){
+		weightSliders("socialwiki_content_area");
+	}
 	//only display tree if in tree view
    if(pattern1.test(document.URL)||!pattern3.test(document.URL)){
 		//add weight sliders
-		weightSliders("socialwiki_content_area");
 		mTree.display();
 		
 		$(window).resize(this, function(e){
